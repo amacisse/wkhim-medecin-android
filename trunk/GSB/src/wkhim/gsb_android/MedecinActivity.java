352a4ba95f316bcbb5b8cca092ef2e610efabced
az.nooob@gmail.com <az.nooob@gmail.com@b80ae716-5179-133c-3bb6-fac18cf8873b>
2014-03-03 09:51:51 +0000

    Début du filtrage

--- trunk/GSB/src/wkhim/gsb_android/MedecinActivity.java
+++ trunk/GSB/src/wkhim/gsb_android/MedecinActivity.java
@@ -8,7 +8,12 @@ package wkhim.gsb_android;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.ArrayList;
 import java.util.List;
 import wkhim.gsb_android.modele.DAO;
 import wkhim.gsb_android.modele.Medecin;
@@ -17,15 +22,29 @@ import wkhim.gsb_android.modele.Medecin;
  *
  * @author wkhim
  */
-public class MedecinActivity extends ListActivity{
+public class MedecinActivity extends ListActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listemed);
         Intent inter = getIntent();
         String dep = inter.getStringExtra("leDep");
-        List<Medecin> lesMeds = DAO.getLesMedsParDep(dep);
+        final List<Medecin> lesMeds = DAO.getLesMedsParDep(dep);
         MedAdapter adapter = new MedAdapter(lesMeds, this);
         setListAdapter(adapter);
+        Button button = (Button) findViewById(R.id.button);
+        final List<Medecin> filtreMeds = new ArrayList<Medecin>();
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                EditText editText = (EditText) findViewById(R.id.search_input);
+                for (Medecin m : lesMeds) {
+                    if (m.getNom().startsWith(editText.getText().toString())) {
+                        filtreMeds.add(m);
+                    }
+                }
+                MedAdapter adapter = new MedAdapter(filtreMeds, this);
+                setListAdapter(adapter);
+            }
+        });
     }
 }
